f047f7b05c509c50629aae960ce3d24fc69efff5
0x009922 <a.marcius26@gmail.com>
2022-01-06 16:52:56 +0300

    fix: typo

--- config/runtime.exs
+++ config/runtime.exs
@@ -2,5 +2,5 @@ import Config
 
 config :cute_fem_bot,
   port: System.get_env("PORT", "3000"),
-  public_path: System.get_env("PUBLIC_PATH")
+  public_path: System.get_env("PUBLIC_PATH"),
   update_approach: System.get_env("UPDATE_APPROACH")
