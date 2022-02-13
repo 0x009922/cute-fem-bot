dc11c1caafeb10a56029d12fd0070b0d55cf6c2c
0x009922 <a.marcius26@gmail.com>
2022-02-13 21:21:06 +0300

    misc: bump version to 1.1.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule CuteFemBot.MixProject do
   def project do
     [
       app: :cute_fem_bot,
-      version: "0.1.0",
+      version: "1.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
