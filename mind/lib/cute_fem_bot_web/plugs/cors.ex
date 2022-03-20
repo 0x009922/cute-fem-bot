2522e529f0e97a9e785d8008999d6c48713425f9
0x009922 <a.marcius26@gmail.com>
2022-03-20 16:09:07 +0300

    fix(cors): another try

--- mind/lib/cute_fem_bot_web/plugs/cors.ex
+++ mind/lib/cute_fem_bot_web/plugs/cors.ex
@@ -3,14 +3,21 @@ defmodule CuteFemBotWeb.Plugs.CORS do
 
   def init(x), do: x
 
-  def call(conn, _) do
+  def call(%Plug.Conn{} = conn, _) do
     %{www: www} = CuteFemBotWeb.Bridge.get_cors_data()
 
-    if www do
-      conn
-      |> put_resp_header("access-control-allow-origin", www)
-    else
-      conn
+    conn =
+      if www do
+        conn
+        |> put_resp_header("access-control-allow-origin", www)
+        |> put_resp_header("access-control-allow-methods", "*")
+      else
+        conn
+      end
+
+    case conn do
+      %Plug.Conn{method: :options} -> send_resp(conn, 204, "")
+      _ -> conn
     end
   end
 end
