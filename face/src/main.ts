b183f884223ade3b97e32579e2f0088e7f2e184d
0x009922 <a.marcius26@gmail.com>
2022-05-01 15:14:47 +0300

    feat: use tg web app script

--- face/src/main.ts
+++ face/src/main.ts
@@ -4,7 +4,12 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
+import '@grammyjs/web-app'
 
 const pinia = createPinia()
 
 createApp(App).use(pinia).use(router).mount('#app')
+
+try {
+  window.Telegram.WebApp.ready()
+} catch {}
